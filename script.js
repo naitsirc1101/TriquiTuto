323571aa8cf18aae8ecf6ed2b822191b907c97f6
Cristian Bohorquez <cristian.bohorquez@inoutdelivery.com>
2020-02-24 23:27:50 -0500

    first Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const cellElements = document.querySelectorAll('[data-cell]')
+
+let circleTurn
+
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true })
+})
+
+function handleClick(e){
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+
+    addClass(cell, currentClass);
+
+    swapTurns();
+}
+
+function addClass(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn
+}
